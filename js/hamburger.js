92fa99cfdb8c58fa3b1844a130df727533f2cac0
Tiffany VACHEZ <tiffany.vachez@toucantoco.com>
2016-08-05 16:31:40 +0200

    update website

---
+++ js/hamburger.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$('#nav-icon').click(function(){
+		$(this).toggleClass('open');
+	});
+});
+
+$(document).ready(function(){
+    $('#nav-icon').click(function(){
+        $(".menu").toggleClass('show');
+    });
+});
